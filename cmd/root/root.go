5988bcde2db43a7287422508c5a77090a52d882e
Oleg Proshin <opp20071980@gmail.com>
2024-12-02 11:45:30 +0300

    removed comment generated by cobra

--- cmd/root/root.go
+++ cmd/root/root.go
@@ -18,10 +18,6 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "chat-cli",
 	Short: "Client application for chat",
-
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
 var loginCmd = &cobra.Command{
@@ -118,20 +114,6 @@ var connectCmd = &cobra.Command{
 }
 
 
-
-/*var createUserCmd = &cobra.Command{
-	Use:   "user",
-	Short: "Создает нового пользователя",
-	Run: func(cmd *cobra.Command, args []string) {
-		usernamesStr, err := cmd.Flags().GetString("username")
-		if err != nil {
-			log.Fatalf("failed to get usernames: %s\n", err.Error())
-		}
-
-		log.Printf("user %s created\n", usernamesStr)
-	},
-}*/
-
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
@@ -142,17 +124,6 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.chat-cli.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-
-	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
 	rootCmd.AddCommand(loginCmd)
 	rootCmd.AddCommand(createChatCmd)
 	rootCmd.AddCommand(connectCmd)
